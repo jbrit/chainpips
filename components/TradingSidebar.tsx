856658b3f5c300e2df147588880b81e7d68b384f
jibola <pro.ajibolaojo@gmail.com>
2022-11-09 21:39:09 +0100

    Include trading sidebar

---
+++ components/TradingSidebar.tsx
@@ -0,0 +1,33 @@
+import React, { useRef } from "react";
+import { Button, Form, Input, Select } from "antd";
+import type { FormInstance } from "antd/es/form";
+
+const { Option } = Select;
+
+type Props = {};
+
+const TradingSidebar = (props: Props) => {
+  const formRef = useRef<FormInstance>(null);
+  const layout = {
+    labelCol: { span: 24 },
+    wrapperCol: { span: 24 },
+  };
+
+  const onChange = (value: string) => {};
+  return (
+    <Form {...layout} ref={formRef} name="control-ref">
+      <Form.Item initialValue={"EURUSD"} name="trading-pair" label="Trading Pair">
+        <Select
+          onChange={onChange}
+          placeholder="Select a option and change input text above"
+        >
+          <Option value="EURUSD">EURUSD</Option>
+          <Option value="GBPUSD">GBPUSD</Option>
+          <Option value="AUDUSD">AUDUSD</Option>
+        </Select>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default React.memo(TradingSidebar);
