cd8e6698cfdd17ac7c75ce727417605dbc35f27b
jibola <pro.ajibolaojo@gmail.com>
2022-11-10 22:52:18 +0100

    added more to positions:

--- components/TradingSidebar.tsx
+++ components/TradingSidebar.tsx
@@ -1,23 +1,44 @@
 import React, { useRef } from "react";
-import { Button, Col, Form, Typography, Row, Select } from "antd";
+import {
+  Button,
+  Col,
+  Form,
+  Typography,
+  Row,
+  Select,
+  Spin,
+  InputNumber,
+} from "antd";
 import type { FormInstance } from "antd/es/form";
 import { useAppContext } from "$utils/context";
-import { getPositions, openBuyTrade } from "$utils/actions";
+import {
+  closeTrade,
+  getPositions,
+  openBuyTrade,
+  openSellTrade,
+} from "$utils/actions";
 import { useWalletInfo } from "$utils/hooks";
+import { useQuery } from "react-query";
 
 const { Option } = Select;
 
 type Props = {};
 
 const TradingSidebar = (props: Props) => {
+  type FormFields = {
+    "trading-pair": string;
+    amount: number;
+  };
   const { setCurrentPair } = useAppContext();
-  const formRef = useRef<FormInstance>(null);
+  const formRef = useRef<FormInstance<FormFields>>(null);
   const layout = {
     labelCol: { span: 24 },
     wrapperCol: { span: 24 },
   };
 
   const { provider, isConnected } = useWalletInfo();
+
+  const positionsQuery = useQuery("positions", () => getPositions(provider));
 
   const onChange = (value: string) => {
     setCurrentPair(value);
@@ -42,28 +63,90 @@ const TradingSidebar = (props: Props) => {
       <Typography.Title style={{ textAlign: "center" }} level={1}>
         0.64121
       </Typography.Title>
+      {/* amount */}
+      <Form.Item
+        name="amount"
+        label="Amount ($)"
+        rules={[{ required: true, message: "Please input amount!" }]}
+      >
+        <InputNumber min={1} />
+      </Form.Item>
+
       <Row gutter={8}>
         <Col span={12}>
           <Button
             type="primary"
             block
             onClick={async () => {
+              await formRef.current?.validateFields();
+              const values = formRef.current?.getFieldsValue();
               if (!isConnected) return alert("Please connect your wallet");
-                await openBuyTrade(provider, 1000);
+              if (!values?.amount) return alert("Please enter amount");
+              await openBuyTrade(provider, values.amount);
+              await positionsQuery.refetch();
             }}
           >
             Buy
           </Button>
         </Col>
         <Col span={12}>
-          <Button type="primary" block danger>
+          <Button
+            type="primary"
+            block
+            onClick={async () => {
+              await formRef.current?.validateFields();
+              const values = formRef.current?.getFieldsValue();
+              if (!isConnected) return alert("Please connect your wallet");
+              if (!values?.amount) return alert("Please enter amount");
+              await openSellTrade(provider, values.amount);
+              await positionsQuery.refetch();
+            }}
+            danger
+          >
             Sell
           </Button>
         </Col>
       </Row>
-      <Typography.Title style={{ padding: "2rem 0" }} level={5}>
-        Positions
+      <Typography.Title style={{ padding: "2rem 0 0" }} level={5}>
+        Open Positions
       </Typography.Title>
+      {positionsQuery.isLoading && <Spin />}
+      {positionsQuery.data
+        ?.filter(({ exitTime }) => exitTime === 0)
+        .map((position) => (
+          <div
+            key={position.id}
+            style={{ marginBottom: "1rem", position: "relative" }}
+          >
+            {/* Display pair, amount, entry price and time, exit price amd time */}
+            <Typography.Title level={5}>
+              {"EURUSD" ?? position.tradingPair},{" "}
+              <span
+                style={{
+                  color: position.tradeType === 0 ? "#1890ff" : "#ff4d4f",
+                }}
+              >
+                {position.tradeType === 0 ? "buy" : "sell"} ${position.amount}
+              </span>
+            </Typography.Title>
+            <div>
+              <Typography.Text>
+                {position.entryPrice} &#8594; {position.exitPrice}{" "}
+              </Typography.Text>
+            </div>
+            <hr />
+            {/* Close */}
+            <Button
+              onClick={async () => {
+                await closeTrade(provider, position.id);
+                await positionsQuery.refetch();
+              }}
+              style={{ position: "absolute", right: "0.5rem", bottom: "50%" }}
+            >
+              x
+            </Button>
+          </div>
+        ))}
     </Form>
   );
 };
